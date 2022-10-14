04ae646d16eae98ec2d72356628ff1af9dd07bce
forrestmorrison <forrestmorrison@gmail.com>
2022-10-13 22:33:51 -0500

    added basic Redux code

---
+++ client/src/features/posts/postsSlice.js
@@ -0,0 +1,6 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+    { id: '1', title: 'Learning Redux Toolkit', content: "I've heard good things."},
+    { id: '1', title: 'Learning Redux Toolkit', content: "I've heard good things."}
+]

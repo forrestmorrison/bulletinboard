70d7c6b4b753985451c686d0ee14d88df8056c89
forrestmorrison <forrestmorrison@gmail.com>
2022-10-13 23:04:40 -0500

    added code to postsSlice.js

--- client/src/features/posts/postsSlice.js
+++ client/src/features/posts/postsSlice.js
@@ -2,5 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
     { id: '1', title: 'Learning Redux Toolkit', content: "I've heard good things."},
-    { id: '1', title: 'Learning Redux Toolkit', content: "I've heard good things."}
+    { id: '2', title: 'Slices...', content: "I want a slice of pizza."}
 ]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {}
+})
+
+export default postsSlice.reducer
